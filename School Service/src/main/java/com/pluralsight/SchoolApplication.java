70962808e2d966b53c1bcb11fca2b899ddb2cfe9
DoulaD <Douladoumbia@gmail.com>
2025-04-18 10:35:59 -0400

    Stringbuilder excercise

---
+++ School Service/src/main/java/com/pluralsight/SchoolApplication.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class SchoolApplication {
+    public static void main(String[] args) {
+        Student student = new student();
+        System.out.println(student);
+
+    }
+}
---
+++ School Service/src/main/java/com/pluralsight/Student.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class Student {
+    private  String firstName = "";
+    private String lastName = "";
+    private int age = 0;
+    private String major = "";
+
+    public Student(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        
+
+    }
+}
