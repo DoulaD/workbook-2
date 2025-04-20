364841823c446ee9d5ee2c49decfd14fe383f6c3
DoulaD <Douladoumbia@gmail.com>
2025-04-20 19:25:01 -0400

    Add Looping Excercise

---
+++ LoopingExcercise/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+   do {
+       System.out.println("I love Java");
+       count = count + 1;
+   } while (count < 5);
+    }
+}
